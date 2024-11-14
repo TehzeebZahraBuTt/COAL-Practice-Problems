5dfea1d8d0271ae1f19fa2ada120c00fffef4b6a
Tehzeeb_Zahra <tehzeebzahrabuttjbd@gmail.com>
2024-11-14 12:32:06 -0800

    array values sum in procedure

--- COALProject02/ArrayElementsStats-usingProcedures.asm
+++ COALProject02/ArrayElementsStats-usingProcedures.asm
@@ -3,85 +3,45 @@ include irvine32.inc
 arr dword ?,?,?,?,?
 arr_elementsInput byte "Enter elements of array : ",0
 additionMsg byte "Press + for addition",0
-subtractionMsg byte "Press - for subtraction",0
-mulMsg byte "Press * for multiplication",0
-maxMsg byte "Press > for finding maximum",0
-minMsg byte "Press < for finding minimum",0
+
 sumres dword ?
 resultSum dword ?
-resultSub dword ?
+
 .code
-arrayStats PROC
+arrayStats PROC uses edx  ecx ebx esi eax
 
 mov edx,offset additionMsg
 call writestring
 call crlf
 
 
-mov edx,offset subtractionMsg
-call writestring
-call crlf
 
 
-mov edx,offset mulMsg
-call writestring
-call crlf
-
-
-mov edx,offset maxMsg
-call writestring
-call crlf
-
-
-mov edx,offset minMsg
-call writestring
-call crlf
-
-call readdec
-;call readchar
-
+;call readdec
+call readchar
+call writechar
 ;labels
-cmp eax,'-'
-jz perform_Subtraction
-jnz codeEnd
-cmp eax,1
-jz perform_Addition
-jnz codeEnd
-cmp eax,'*'
-jz perform_Multiplication
-jnz codeEnd
-cmp eax,'>'
-jz perform_maxCalculation
-jnz codeEnd
-cmp eax,'<'
-jz perform_minCalculation
-jnz codeEnd
+
+cmp al,'+'
+je perform_Addition
+
 
 
 
 perform_Addition:
-	mov edx,offset arr
+	;mov edx,offset arr
 	mov eax,0
-	mov ecx,type arr
+	mov ecx,lengthof arr
 	mov ebx,0
 		addd:
 
-		add eax, [edx+ebx*type arr]
+		add eax, [esi+ebx*type arr]
 		inc ebx
 
 		loop addd
 	mov sumres,eax
+jmp codeEnd
 
-perform_subtraction:
-
-
-perform_multiplication:
-
-
-perform_maxCalculation:
-
-
-perform_minCalculation:
 
 codeEnd:
 RET
@@ -97,14 +57,15 @@ mov edx,offset arr
 	input_values:
 	call readdec
 
-	mov [edx+ebx*type arr],ax
+	mov [edx+ebx*type arr],eax
 	inc ebx
 
 	loop input_values
-
+mov esi,offset arr
 call arrayStats	
 mov eax,sumres
 call writedec
+
 exit
 main endp
 end main
