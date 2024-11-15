64497b93ad827f3aaf6d120f3514220cb08d4817
Tehzeeb_Zahra <tehzeebzahrabuttjbd@gmail.com>
2024-11-15 14:28:56 +0500

    procedures using stack

---
+++ COALProject02/arrayStats-usingStack.asm
@@ -0,0 +1,80 @@
+include irvine32.inc
+.data
+arr dword ?,?,?,?,?
+arr_elementsInput byte "Enter elements of array : ",0
+additionMsg byte "Press + for addition",0
+
+sumres dword ?
+resultSum dword ?
+
+.code
+arrayStats PROC 
+push ebp
+mov ebp,esp
+mov edx,[ebp+16]
+call writestring
+call crlf
+
+
+
+
+;call readdec
+call readchar
+call writechar
+;labels
+
+cmp al,'+'
+je perform_Addition
+
+
+
+
+perform_Addition:
+	;mov edx,offset arr
+	mov esi,[ebp+12]
+
+	mov eax,0
+	mov ecx,[ebp+8]
+	mov ebx,0
+		addd:
+
+		add eax, [esi+ebx*type arr]
+		inc ebx
+
+		loop addd
+	mov sumres,eax
+jmp codeEnd
+
+
+codeEnd:
+pop ebp
+RET
+arrayStats endp
+main proc
+
+mov edx,offset arr_elementsInput
+call writestring
+mov ebx,0
+mov ecx,lengthof arr
+mov edx,offset arr
+;input arrqay elements
+	input_values:
+	call readdec
+
+	mov [edx+ebx*type arr],eax
+	inc ebx
+
+	loop input_values
+
+;----arguments
+push offset additionmsg
+push offset arr
+push lengthof arr
+;function call
+call arrayStats	
+mov eax,sumres
+call writedec
+
+exit
+main endp
+end main
