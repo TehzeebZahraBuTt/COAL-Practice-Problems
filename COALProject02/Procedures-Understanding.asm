8ee4cc90928894682041d3bd6a35a3af24445bfe
Tehzeeb_Zahra <tehzeebzahrabuttjbd@gmail.com>
2024-11-15 08:48:28 +0500

    basic-procedures

---
+++ COALProject02/Procedures-Understanding.asm
@@ -0,0 +1,24 @@
+include irvine32.inc
+.data
+msg byte "just msg",0
+.code
+
+AddTwo PROC
+ push ebp
+ mov ebp,esp
+ mov eax,[ebp+12]
+ add eax,[ebp+8]
+ call writedec
+ pop ebp
+ ret
+ AddTwo ENDP
+main proc
+
+
+push 5
+push 6
+call addTwo
+
+exit
+main endp
+end main
